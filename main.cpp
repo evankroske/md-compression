15682bc1306c9e93ea80d2db2f0ba2c2b4703bdd
Evan Kroske <evan@evankroske.com>
2012-01-28 23:59:12 -0500

    Simplified enqueue_to_lsb and added constructors to Coordinate and OctreeIndexParams.

--- main.cpp
+++ main.cpp
@@ -3,42 +3,39 @@
 using namespace std;
 
 typedef unsigned int uint;
-typedef struct {
+struct Coordinate {
 uint x;
 uint y;
 uint z;
-} Coordinate;
+Coordinate(uint _x, uint _y, uint _z): x(_x), y(_y), z(_z) {};
+};
 
-typedef struct {
+struct OctreeIndexParams{
 int x_width;
 int y_width;
 int z_width;
-} OctreeIndexParams;
+OctreeIndexParams (int _x_width, int _y_width, int _z_width): 
+	x_width(_x_width), y_width(_y_width), z_width(_z_width) {};
+};
 
 const uint MSB = 1 << (sizeof(uint) * 8 - 1);
 FILE *log_file;
 
 void print_coordinate (Coordinate c);
 uint octree_index (Coordinate c, OctreeIndexParams *p);
-uint top_bit (uint i);
-void transfer_msb (uint *a, uint *b);
 void var_encode_uint(uint a, int l, int d_l, char *bu);
 uint lsb_bitmask (int l);
-void enqueue_to_lsb (uint src, uint bitmask, uint *dst);
+void enqueue_to_lsb (uint src, int bit_index, uint *dst);
 
 int main ()
 {
-	log_file = fopen("log.txt", "w");
+	// log_file = fopen("log.txt", "w");
+	log_file = stdout;
 
-	Coordinate c;
-	c.x = 2;
-	c.y = 1;
-	c.z = 1;
-	
-	OctreeIndexParams p;
-	p.x_width = 2;
-	p.y_width = 2;
-	p.z_width = 2;
+	Coordinate c(2, 1, 1);
+	OctreeIndexParams p(2, 2, 2);
+	uint o_i = octree_index(c, &p);
+	fprintf(log_file, "octree_index = %u\n", o_i);
 
 	return 0;
 }
@@ -51,29 +48,14 @@ void print_coordinate (Coordinate c)
 uint octree_index (Coordinate c, OctreeIndexParams *p)
 {
 	uint octree_index = 0;
-	int i = 32;
-	uint m = 1 << (sizeof(uint) * 8 - 1);
-	for (; i != 0 && m != 0; i--, m >>= 1)
+	for (int i = sizeof(uint) * 8 - 1; i >= 0; i--)
 	{
-		if (i <= p->x_width) enqueue_to_lsb(c.x, m, &octree_index);
-		if (i <= p->y_width) enqueue_to_lsb(c.y, m, &octree_index);
-		if (i <= p->z_width) enqueue_to_lsb(c.z, m, &octree_index);
+		if (i <= p->x_width) enqueue_to_lsb(c.x, i, &octree_index);
+		if (i <= p->y_width) enqueue_to_lsb(c.y, i, &octree_index);
+		if (i <= p->z_width) enqueue_to_lsb(c.z, i, &octree_index);
 	}
 
 	return octree_index;
-}
-
-uint top_bit (uint i)
-{
-	return (i & MSB) ? 1 : 0;
-}
-
-void transfer_msb (uint *a, uint *b)
-{
-	fprintf(log_file, "transfer_msb(a = %u; b = %u)\n", *a, *b);
-	*b <<= 1;
-	*b |= top_bit(*a);
-	*a <<= 1;
 }
 
 void var_encode_uint(uint a, int l, int d_l, char *bu)
@@ -93,9 +75,9 @@ uint lsb_bitmask (int l)
 	return mask;
 }
 
-void enqueue_to_lsb (uint src, uint bitmask, uint *dst)
+void enqueue_to_lsb (uint src, int bit_index, uint *dst)
 {
-	fprintf(log_file, "enqueue_to_lsb(src = %d, bitmask = %u, dst = %u)\n", src, bitmask, *dst);
+	fprintf(log_file, "enqueue_to_lsb(src = %d, bit_index = %d, dst = %u)\n", src, bit_index, *dst);
 	*dst <<= 1;
-	*dst |= (src & bitmask) ? 1 : 0;
+	*dst |= (src >> bit_index) & 1;
 }
