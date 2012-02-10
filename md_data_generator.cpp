82868386d7752b9ff7b09e4387211351c74e3008
Evan Kroske <evan@evankroske.com>
2012-02-10 00:57:45 -0500

    Made generator produce smaller numbers to prevent overflow.

--- md_data_generator.cpp
+++ md_data_generator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <ctime>
+#include "omeltchenko99.h"
 
+const int MAX_BITS = 21;
 int main (int argc, char **argv)
 {
 	int num_coordinates = 100;
@@ -8,11 +10,12 @@ int main (int argc, char **argv)
 		puts(argv[1]);
 		sscanf(argv[1], "%d", &num_coordinates);
 	}
+	int max_bits = MAX_BITS;
 
 	srand(time(NULL));
 	for (int i = 0; i < num_coordinates; i++)
 	{
-		printf("%d\t%d\t%d\n", rand(), rand(), rand());
+		printf("%u\t%u\t%u\n", rand() & lsb_bitmask<int>(max_bits), rand() & lsb_bitmask<int>(max_bits), rand() & lsb_bitmask<int>(max_bits));
 	}
 
 	return 0;
